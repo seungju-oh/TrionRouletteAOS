a9ee479aeeb7f735c7a22f6541ac5b7d8421ee2d
Leuchten <susumu.hirasawa247@gmail.com>
2026-05-19 16:33:34 +0900

    Version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.torimaru.trionroulette"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
